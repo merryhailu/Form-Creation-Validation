f22b08fe3598f8d34b806e6fddbcef4409b548b5
Meron Hailu <hailumeron03@gmail.com>
2024-07-18 14:57:44 +0300

    second task

---
+++ fetch-data.js
@@ -0,0 +1,32 @@
+
+async function fetchUserData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById('api-data');
+
+    try {
+        const response = await fetch(apiUrl);
+        const user = await response.json();
+
+        dataContainer.innerHTML = '';
+
+        const userList = document.createElement('ul');
+        const users = [];
+
+        users.forEach(user => {
+            const listItem = document.createElement('li');
+            listItem.textContent = user.name;
+            userList.appendChild(listItem);
+        });
+
+        dataContainer.appendChild(userList);
+    }
+
+    catch (error) {
+        dataContainer.innerHTML = '';
+        dataContainer.textContent = 'Failed to load user data.';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchUserData();
+});
