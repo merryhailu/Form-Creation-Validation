0032293a31c6e61b627cf4c06d46eb82b1f78573
Meron Hailu <hailumeron03@gmail.com>
2024-07-18 13:44:45 +0300

    v1

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
 
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
 
 
